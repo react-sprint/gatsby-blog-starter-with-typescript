2cc6868bc6610dd271421d84a0e3a080a4d0fa3a
sungyun <sungyun5423@gmail.com>
2021-03-23 23:01:46 +0900

    file extension js -> tsx change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Link, graphql } from 'gatsby';
 
 import Bio from '../components/bio';
-import Layout from '../components/layout/index.js';
+import Layout from '../components/layout/index';
 import SEO from '../components/seo';
 import Sample from '../components/sample';
 
