86272147657901747ab541c351d945b9f7c03aa5
sungyun <sungyun5423@gmail.com>
2021-03-30 22:24:58 +0900

    feat: GitButton, Icon Component 추가

---
+++ src/components/common/GitButton.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../styles/components/git-button.scss';
+
+const GitButton = () => <button className="git-button">GITHUB</button>;
+
+export default GitButton;
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 
 import './index.scss';
+import GitButton from '../common/GitButton';
 
 const Layout = ({ location, title, children }) => {
   let header;
@@ -25,7 +26,9 @@ const Layout = ({ location, title, children }) => {
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       {/* <header className="global-header">{header}</header> */}
-      <header className="global-header">header</header>
+      <header className="global-header">
+        <GitButton />
+      </header>
       <main className="global-main">{children}</main>
       {/* <footer>
         © {new Date().getFullYear()}, Built with
