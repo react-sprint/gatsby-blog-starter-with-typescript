731f3472c6a405a18cdffc5fc5cad3308c836f1b
sungyun <sungyun5423@gmail.com>
2021-05-02 16:45:07 +0900

    fix: description

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -53,26 +53,26 @@ export default function Card({ post }: Props) {
 }
 
 // 기존 소스 참고용
-{
-  /* <li key={post.fields.slug}>
-  <article className="post-list-item" itemScope itemType="http://schema.org/Article">
-    <header>
-      <h2>
-        <Link to={post.fields.slug} itemProp="url">
-          <span itemProp="headline">{title}</span>
-        </Link>
-      </h2>
-      <small>{post.frontmatter.date}</small>
-    </header>
-    <section>
-      <p
-        dangerouslySetInnerHTML={{
-          __html: post.frontmatter.description || post.excerpt,
-        }}
-        itemProp="description"
-      />
-    </section>
-  </article>
-  <Sample />
-</li> */
-}
+// {
+//   <li key={post.fields.slug}>
+//     <article className="post-list-item" itemScope itemType="http://schema.org/Article">
+//       <header>
+//         <h2>
+//           <Link to={post.fields.slug} itemProp="url">
+//             <span itemProp="headline">{title}</span>
+//           </Link>
+//         </h2>
+//         <small>{post.frontmatter.date}</small>
+//       </header>
+//       <section>
+//         <p
+//           dangerouslySetInnerHTML={{
+//             __html: post.frontmatter.description || post.excerpt,
+//           }}
+//           itemProp="description"
+//         />
+//       </section>
+//     </article>
+//     <Sample />
+//   </li>;
+// }
