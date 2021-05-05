1d7e02cd2c24d89d1bf67217d1e1a4d0ab2f3060
leeyun1533 <leeyun1533@naver.com>
2021-05-06 02:33:22 +0900

    feat: add post footer

---
+++ src/components/common/PostFooterCard.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import '../../styles/components/common/post-footer-card.scss';
+
+interface Field {
+  slug: string;
+}
+interface Frontmatter {
+  title: string;
+  description: string;
+  date: string;
+}
+
+interface Post {
+  excerpt: string;
+  frontmatter: Frontmatter;
+  fields: Field;
+}
+
+interface Props {
+  post: Post;
+  thumbnail: string | null;
+}
+
+export default function PostFooterCard({ post, thumbnail }: Props) {
+  const title = post.frontmatter.title.split(' ')[0] || post.fields.slug;
+  const themeSize = 3;
+  const themeStartNumber = 1;
+
+  return (
+    <Link to={post.fields.slug}>
+      <div className="post-footer-card">
+        <div className="card">
+          <div className="left">
+            <h2 className="title">{title}</h2>
+            <p className="contents">{post.frontmatter.description || post.excerpt}</p>
+          </div>
+          {thumbnail ? (
+            <div className="right" dangerouslySetInnerHTML={{ __html: thumbnail }} />
+          ) : (
+            <div
+              className={`right no-thumbnail theme-color-${Math.floor(Math.random() * themeSize + themeStartNumber)}`}
+            >
+              <span>{title}</span>
+            </div>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,17 +1,37 @@
 import * as React from 'react';
 import { Link, graphql } from 'gatsby';
 
-import Bio from '../components/Bio';
 import Layout from '../components/layout';
 import arrowIcon from '../images/arrow.svg';
 import SEO from '../components/Seo';
+import prevIcon from '../images/post-prev-icon.svg';
+import nextIcon from '../images/post-next-icon.svg';
 import '../styles/blog-post.scss';
 import '../styles/code.scss';
+import PostFooterCard from '../components/common/PostFooterCard';
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark;
+  const posts = data.allMarkdownRemark.nodes;
   const siteTitle = data.site.siteMetadata?.title || `Title`;
+  const { category } = data.markdownRemark.frontmatter;
+
   const { previous, next } = data;
+
+  const getThumbnail = (postIndex) => {
+    const regex = /<img[^>]+src\s*=\s*['"]([^'"]+)['"][^>]*>/g;
+    const htmlString = data.allMarkdownRemark.nodes.map((htmlCode, htmlIndex) => {
+      if (postIndex === htmlIndex) {
+        return htmlCode.html;
+      }
+      return null;
+    });
+    const image = regex.exec(htmlString);
+    if (image && image.length) {
+      return `${image[0].split('srcset')[0]}/>`;
+    }
+    return null;
+  };
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -19,42 +39,40 @@ const BlogPostTemplate = ({ data, location }) => {
       <article className="blog-post" itemScope itemType="http://schema.org/Article">
         <header>
           <p className="category">
-            <span>HOME</span> <img src={arrowIcon} /> <span>카테고리명</span>
+            <span>HOME</span> <img src={arrowIcon} /> <span>{category}</span>
           </p>
           <h1 itemProp="headline">{post.frontmatter.title}</h1>
           <p>SubTitle {post.frontmatter.date}</p>
         </header>
         <section dangerouslySetInnerHTML={{ __html: post.html }} itemProp="articleBody" />
-        <hr />
-        <footer>
-          <Bio />
-        </footer>
       </article>
       <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
+        <ul>
           <li>
             {previous && (
               <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
+                <img src={prevIcon} className="left" /> PREV
               </Link>
             )}
           </li>
           <li>
             {next && (
               <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
+                RIGHT <img src={nextIcon} className="right" />
               </Link>
             )}
           </li>
         </ul>
+        <p className="category">
+          <span>HOME</span> <img src={arrowIcon} /> <span>{data.markdownRemark.frontmatter.category}</span>
+        </p>
+        <div className="post-card-container">
+          {posts
+            .filter((item) => category === item.frontmatter.category)
+            .map((item, postIndex) => (
+              <PostFooterCard key={item.fields.slug} post={item} thumbnail={getThumbnail(postIndex)} />
+            ))}
+        </div>
       </nav>
     </Layout>
   );
@@ -77,6 +95,7 @@ export const pageQuery = graphql`
         title
         date(formatString: "MMMM DD, YYYY")
         description
+        category
       }
     }
     previous: markdownRemark(id: { eq: $previousPostId }) {
@@ -93,6 +112,24 @@ export const pageQuery = graphql`
       }
       frontmatter {
         title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      group(field: frontmatter___category) {
+        fieldValue
+      }
+      nodes {
+        excerpt
+        html
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+          category
+        }
       }
     }
   }
